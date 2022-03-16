aff22c0f3dcd9ec59c5ce38298aefebe3b53ba0f
binhpt-1239 <pham.thanh.binh@sun-asterisk.com>
2022-03-16 14:45:09 +0700

    Log In/Out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
     get "static_pages/home"
     get "static_pages/help"
     get "static_pages/about"
+    get "login" => "sessions#new"
+    post "login" => "sessions#create"
+    delete "logout" => "sessions#destroy"
   end
 end
