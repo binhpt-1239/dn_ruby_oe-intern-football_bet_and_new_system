2a0ec26bd91f68dae2bf7e9ff42f8095e1785a20
binhpt-1239 <pham.thanh.binh@sun-asterisk.com>
2022-03-21 12:42:35 +0700

    Model Currencies & Seed

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,12 @@
 class Currency < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :currency_type
+
+  scope :newest, ->{order created_at: :desc}
+  scope :search_by_type, ->(type){where currency_type_id: type if type.present?}
+
+  enum currency_type_id: {payment: 1, withdrawal: 2, win: 3, lose: 4}
+
+  validates :amount, presence: true, numericality: {greater_than_or_equal_to: 0,
+                                                    only_integer: true}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,61 +1,76 @@
 User.create!(name: "Example User",
-             email: "example@railstutorial.org",
-             password: "123456",
-             password_confirmation: "123456",
-             admin: true)
+  email: "example@railstutorial.org",
+  password: "123456",
+  password_confirmation: "123456",
+  admin: true)
 
 15.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
+name = Faker::Name.name
+email = "example-#{n+1}@railstutorial.org"
+password = "password"
+User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password)
 end
 
 50.times do |n|
-  name = Faker::Sports::Football.player
-  position = Faker::Sports::Football.position
-  number = rand(1..35)
-  Player.create!(name: name,
-                 position: position,
-                 number: number)
+name = Faker::Sports::Football.player
+position = Faker::Sports::Football.position
+number = rand(1..35)
+Player.create!(name: name,
+      position: position,
+      number: number)
 end
 
 5.times do |n|
-  name = Faker::Sports::Football.team
-  Team.create!(name: name)
+name = Faker::Sports::Football.team
+Team.create!(name: name)
 end
 
 Season.create!(name: "2021 - 2022",
-               begin_year: 2021,
-               end_year: 2022)
+    begin_year: 2021,
+    end_year: 2022)
 
 Tournament.create!(name: "BK-TB",
-                   begin_time: "2021-06-15 00:30:00",
-                   end_time: "2022-06-15 00:30:00")
+        begin_time: "2021-06-15 00:30:00",
+        end_time: "2022-06-15 00:30:00")
 
 SeasonTournament.create!(season_id: 1,
-                         tournament_id: 1)
+              tournament_id: 1)
 
 5.times do |n|
-  TeamSeasonTournament.create!(team_id: n+1,
-                     season_tournament_id: 1)
+TeamSeasonTournament.create!(team_id: n+1,
+          season_tournament_id: 1)
 end
 
-currency_type = ["win", "lose", "recharge", "withdraw", "transfer"]
+currency_type = ["recharge", "withdraw", "win", "lose", "transfer"]
 5.times do |n|
-  CurrencyType.create!(name: currency_type[n])
+CurrencyType.create!(name: currency_type[n])
+end
+
+10.times do |i|
+Currency.create!(amount: 200,
+user_id: 1,
+currency_type_id: 1)
+Currency.create!(amount: 150,
+user_id: 1,
+currency_type_id: 2)
+Currency.create!(amount: 50,
+user_id: 1,
+currency_type_id: 3)
+Currency.create!(amount: 45,
+user_id: 1,
+currency_type_id: 4)
 end
 
 random_id = (1..50).to_a.shuffle
 50.times do |n|
-  player_id = random_id[n]
-  team_id = n / 10
-  team_id = 5 if team_id == 0
-  PlayerInfo.create!(player_id: player_id,
-                     team_id: team_id,
-                     season_tournament_id: 1
-  )
+player_id = random_id[n]
+team_id = n / 10
+team_id = 5 if team_id == 0
+PlayerInfo.create!(player_id: player_id,
+          team_id: team_id,
+          season_tournament_id: 1
+)
 end
