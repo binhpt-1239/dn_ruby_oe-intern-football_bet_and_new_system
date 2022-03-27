b33eb96d2f8fe5b0144f3d204ac79ed876d4b972
binhpt-1239 <pham.thanh.binh@sun-asterisk.com>
2022-03-27 23:08:05 +0700

    Seed data and modify any code

--- app/controllers/admin/user_bets_controller.rb
+++ app/controllers/admin/user_bets_controller.rb
@@ -1,14 +1,9 @@
 class Admin::UserBetsController < Admin::BaseController
   include SoccerMatchHelper
 
-  before_action :load_match, :check_status_match, only: :index
+  before_action :load_match, :load_bet, :check_status_match, only: :index
 
   def index
-    @bet = if params[:score]
-             @match.bets.find_by bet_type: load_result_score(@match)
-           else
-             @match.bets.find_by bet_type: load_result(@match)
-           end
     user_bets = @bet.user_bets.newest
     @pagy, @user_bets = pagy user_bets, items: Settings.digits.digit_6
   end
@@ -20,6 +15,18 @@ class Admin::UserBetsController < Admin::BaseController
     return if @match
 
     flash[:warning] = t ".not_found_match"
+    redirect_to admin_root_path
+  end
+
+  def load_bet
+    @bet = if params[:score]
+             @match.bets.find_by bet_type: load_result_score(@match)
+           else
+             @match.bets.find_by bet_type: load_result(@match)
+           end
+    return if @bet
+
+    flash[:warning] = t ".not_found_bet"
     redirect_to admin_root_path
   end
 
--- app/models/team_tournament.rb
+++ app/models/team_tournament.rb
@@ -1,4 +1,6 @@
 class TeamTournament < ApplicationRecord
   belongs_to :team
   belongs_to :tournament
+  has_many :player_infos, dependent: :destroy
+  has_many :players, through: :player_infos
 end
