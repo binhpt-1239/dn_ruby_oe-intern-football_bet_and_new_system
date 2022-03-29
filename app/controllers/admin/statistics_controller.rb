a5c158241c0b1727d3925af5714cf8b0c6dd3b3c
BaoKhanh99 <tlbk1999@gmail.com>
2022-03-29 20:26:52 +0700

    statistic for a period
    statistic for a period

---
+++ app/controllers/admin/statistics_controller.rb
@@ -0,0 +1,39 @@
+class Admin::StatisticsController < Admin::BaseController
+  include EventsHelper
+
+  before_action :check_year, only: :index
+
+  def index
+    @start = fix_date(params[:start_month])
+    @end = fix_date(params[:end_month])
+    @user_bets = UserBet.search @start.beginning_of_month, @end.end_of_month
+    load_total @user_bets
+  end
+
+  private
+
+  def fix_date param_date
+    if param_date.present?
+      Date.new(@year,
+               param_date["date(2i)"].to_i,
+               param_date["date(3i)"].to_i)
+    else
+      Time.zone.today
+    end
+  end
+
+  def check_year
+    @year = if params[:years]
+              params[:years]["date(1i)"].to_i
+            else
+              Time.zone.now.year
+            end
+  end
+
+  def load_total user_bets
+    winners = user_bets.load_result_bet :win
+    @total_winner = total_win_bet winners
+    @total_loser = user_bets.load_result_bet(:lose).sum(:amount)
+    @total = user_bets.sum(:amount)
+  end
+end
