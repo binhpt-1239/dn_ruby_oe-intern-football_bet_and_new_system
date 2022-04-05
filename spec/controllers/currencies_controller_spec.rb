7ba405da64572d0b97a3bbadfae3463fe40a6441
binhpt-1239 <pham.thanh.binh@sun-asterisk.com>
2022-04-05 23:16:33 +0700

    Rspec for controller Curriencies

---
+++ spec/controllers/currencies_controller_spec.rb
@@ -0,0 +1,90 @@
+require "rails_helper"
+include SessionsHelper
+
+RSpec.describe CurrenciesController, type: :controller do
+  let!(:user){FactoryBot.create :user}
+  describe "GET index" do
+    context "success when logged in" do
+      before do
+        log_in user
+        get :index
+      end
+
+      it "set currencies" do
+        currencies = current_user.currencies.search_by_type(nil)
+        expect(assigns(:currencies)).to eq currencies
+      end
+      it_behaves_like "render action template", "index"
+    end
+
+    it_behaves_like "when not logged in", "index"
+  end
+
+  describe "GET new" do
+    context "success when logged in" do
+      before do
+        log_in user
+        get :new
+      end
+
+      it "set currencies user" do
+        expect(assigns(:currency)).to be_a_new(Currency)
+      end
+      it_behaves_like "render action template", "new"
+    end
+
+    it_behaves_like "when not logged in", "new"
+  end
+
+  describe "POST create" do
+    let(:valid_params){{currency: {amount: 1000, currency_type_id: "payment"}}}
+    let(:type_invalid_params){{currency: {amount: 1000, currency_type_id: "invalid"}}}
+    let(:amount_invalid_params){{currency: {amount: 1000, currency_type_id: "withdrawal"}}}
+    context "success with valid params" do
+      let!(:count){Currency.count}
+      before do
+        log_in user
+        post :create, params: valid_params
+      end
+
+      it "creates a new Currency" do
+        expect(Currency.count).to eq(count+1)
+      end
+      it_behaves_like "flash success message", "currencies.create.success"
+      it_behaves_like "redirect to path", "currencies_path"
+    end
+
+    it_behaves_like "when not logged in", "create"
+
+    context "fails when save fails " do
+      before do
+        log_in user
+        allow_any_instance_of(Currency).to receive(:save).and_return(false)
+        post :create, params: valid_params
+      end
+
+      it_behaves_like "flash warning message", "currencies.create.fails"
+      it_behaves_like "redirect to path", "currencies_path"
+    end
+
+    context "fails when type invalid" do
+      before do
+        log_in user
+        post :create, params: type_invalid_params
+      end
+
+      it_behaves_like "flash warning message", "currencies.create.fails"
+      it_behaves_like "redirect to path", "currencies_path"
+    end
+
+    context "fails when withdrawal invalid amount" do
+      before do
+        log_in user
+        post :create, params: amount_invalid_params
+      end
+
+      it_behaves_like "flash warning message", "currencies.amount_invalid"
+      it_behaves_like "redirect to path", "currencies_path"
+    end
+  end
+end
--- spec/shared_example/controller_spec.rb
+++ spec/shared_example/controller_spec.rb
@@ -1,3 +1,16 @@
+RSpec.shared_examples "when not logged in" do |action|
+  before {get action}
+
+  it_behaves_like "flash danger message", "notification.log_in.request"
+  it_behaves_like "redirect to path", "login_path"
+end
+
+RSpec.shared_examples "render action template" do |action|
+  it "render #{action} template" do
+    expect(response).to render_template action
+  end
+end
+
 RSpec.shared_examples "flash warning message" do |message|
   it "flash warning" do
     expect(flash[:warning]).to eq I18n.t(message, default: message)
