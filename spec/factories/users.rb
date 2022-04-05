a092abcd333ca362e2f7bf0620de82195a4e00ff
binhpt-1239 <pham.thanh.binh@sun-asterisk.com>
2022-04-05 14:55:58 +0700

    Rspec controller Session

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,12 @@ FactoryBot.define do
     password {"123456"}
     password_confirmation {"123456"}
     admin {1}
+    remember_digest{0}
+
+    trait :client do
+      admin {0}
+    end
+
+    factory :client_user, class: User, traits: [:client]
   end
 end
